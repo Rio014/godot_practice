9af00dcac6279574dcdf77fe40810ab570c4666e
Rio014 <rio014.null@gmail.com>
2026-04-19 20:01:25 -0500

    Added hurt animation+logic

--- fox-antics/Scenes/Player/player.gd
+++ fox-antics/Scenes/Player/player.gd
@@ -8,14 +8,23 @@ const GRAVITY: float = 690.0
 const JUMP_SPEED: float = -270.0
 const RUN_SPEED: float = 120.0
 const MAX_FALL_VELOCITY: float = 350.0
+const HURT_JUMP_VELOCITY: Vector2 = Vector2(0, -130.0) 
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var debug_label: Label = $DebugLabel
 
 
 @onready var shooter: Shooter = $Shooter
-@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+@onready var sound: AudioStreamPlayer2D = $Sound
 
+@onready var remain_hurt_timer: Timer = $RemainHurtTimer
+
+# audio stream
+const JUMP = preload("res://assets/sound/jump.wav")
+const DAMAGE = preload("res://assets/sound/damage.wav")
+
+
+var is_hurt: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -40,7 +49,8 @@ func _physics_process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and is_on_floor():
 		velocity.y += JUMP_SPEED # not multiply delta since move_and_slide() already handled that
-		jump_sound.play()
+		#sound.play()
+		play_effect(JUMP)
 		
 	# test shooting (delete later)
 	if  event.is_action_pressed("shoot"):
@@ -54,6 +64,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 
 func player_move() -> void:
+	if is_hurt:
+		return # no movement if player is hurt
 	var player_dir: float = Input.get_axis("left", "right")
 	if !is_equal_approx(velocity.x, 0.0): # only flip when we move
 		sprite_2d.flip_h = velocity.x < 0 # flip h if negative
@@ -72,3 +84,31 @@ func debug_show() -> void:
 	var debug_str: String
 	debug_str = "Floor: %s\nvelocity:%s\nglobal_pos:%s" % [is_on_floor(), velocity, global_position]
 	debug_label.text = debug_str
+
+func apply_hit() -> void:
+	apply_hurt_jump()
+	
+func apply_hurt_jump() -> void:
+	is_hurt = true
+	remain_hurt_timer.start()
+	velocity = HURT_JUMP_VELOCITY
+	play_effect(DAMAGE)
+	
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	# we are setting hurt jump velocity during the time we receive this signal during the frame
+	# so we call deferred to avoid issue with the physics
+	call_deferred("apply_hit")
+
+	
+
+
+func _on_remain_hurt_timer_timeout() -> void:
+	is_hurt = false
+
+
+# switch existing sound effect with the given effect
+func play_effect(effect: AudioStream) -> void:
+	sound.stop()
+	sound.stream = effect
+	sound.play()
+	
