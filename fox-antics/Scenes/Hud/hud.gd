18b5cb3acd3a03802dc18bfe6bca51c4a97143b7
Rio014 <rio014.null@gmail.com>
2026-04-23 22:06:46 -0500

    Added score display in main scene

--- fox-antics/Scenes/Hud/hud.gd
+++ fox-antics/Scenes/Hud/hud.gd
@@ -7,11 +7,20 @@ var current_score: int = 0
 func _enter_tree() -> void:
 	SignalHub._on_increase_score.connect(update_score)
 
+func _exit_tree() -> void:
+	GameManager.try_add_new_score(current_score)
+
+func _ready() -> void:
+	current_score =  GameManager.cached_score
+	update_score(0) # update the label
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("quit"):
 		GameManager.load_main()
+		
 
 
 func update_score(score: int) -> void:
 	current_score += score
 	score_label.text  = "%04d" % [current_score]
+	
--- fox-antics/Scenes/Main/main.gd
+++ fox-antics/Scenes/Main/main.gd
@@ -1,7 +1,20 @@
 extends Control
 
+const HIGH_SCORE_DISPLAY_V_BOX = preload("res://Scenes/HighScoreDisplay/HighScoreDisplayVBox.tscn")
+@onready var grid_container: GridContainer = $MarginContainer/GridContainer
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_SPACE):
 		GameManager.load_next_level()
 		
+func _ready() -> void:
+	set_scores()
+
+func set_scores() -> void:
+	# loop through the score lists we have
+	for score: HighScore in GameManager.high_scores.get_scores_list():
+		# instantiate new high score display object
+		var new_high_score_display: HighScoreDisplayItem = HIGH_SCORE_DISPLAY_V_BOX.instantiate()
+		new_high_score_display.setup(score)
+		grid_container.add_child(new_high_score_display)
